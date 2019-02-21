d0ccafbda744d2c712305ad7d0404871a3eed0a5
edoapra <edoardo.apra@gmail.com>
2019-02-21 10:48:32 -0800

    use icl.utk svn repository for scalapack

--- travis/build_scalapack.sh
+++ travis/build_scalapack.sh
@@ -3,8 +3,9 @@ source $TRAVIS_BUILD_DIR/travis/nwchem.bashrc
 if [[ "$USE_64TO32" != "y" ]] ; then exit 0; fi
 cd $TRAVIS_BUILD_DIR
 os=`uname`
+svn co  https://icl.utk.edu/svn/scalapack-dev/scalapack/trunk/ scalapack
 #git clone https://github.com/scibuilder/scalapack.git
-git clone https://github.com/edoapra/scalapack.git 
+#git clone https://github.com/edoapra/scalapack.git 
 mkdir -p scalapack/build
 cd scalapack/build
 cmake -Wno-dev ../ -DCMAKE_BUILD_TYPE=RelWithDebInfo -DTEST_SCALAPACK=OFF  -DBUILD_TESTING=OFF -DBUILD_SHARED_LIBS=ON  -DBLAS_blas_LIBRARY="-L$TRAVIS_BUILD_DIR -lopenblas" -DLAPACK_lapack_LIBRARY="-L$TRAVIS_BUILD_DIR  -lopenblas"  -DCMAKE_INSTALL_PREFIX=$TRAVIS_BUILD_DIR
